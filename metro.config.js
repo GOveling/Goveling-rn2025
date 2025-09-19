37274497966a0723585641dac81dbf7aa6c0f2d1
GOveling <info@goveling.com>
2025-09-19 07:40:05 -0300

    Fix Metro configuration conflicts and rebuild

--- metro.config.js
+++ metro.config.js
@@ -2,15 +2,6 @@ const { getDefaultConfig } = require('expo/metro-config');
 
 const config = getDefaultConfig(__dirname);
 
-// Resolver optimizations for path aliases
-config.resolver = {
-  ...config.resolver,
-  alias: {
-    '~': './src',
-    '@': './src',
-  },
-};
-
 // Performance optimizations (compatible with Bolt)
 if (process.env.NODE_ENV === 'production') {
   config.transformer.minifierConfig = {
